0c769b20fc25cc6678f2fef1002547bc9842e66a
unknown <buendia732@gmail.com>
2022-12-07 22:10:31 -0600

    Prueba

---
+++ Server.js
@@ -0,0 +1,33 @@
+var http = require('http').createServer(handler); //require http server, and create server with function handler()
+var fs = require('fs'); //require filesystem module
+var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
+
+http.listen(8080); //listen to port 8080
+
+function handler (req, res) { //create server
+  fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+    res.write(data); //write data from index.html
+    return res.end();
+  });
+}
+
+io.sockets.on("connection", (socket) => {
+    socket.emit('hello', "Conectado al servidor");
+
+    socket.on('howdy', (arg) => {
+        console.log(arg);
+    });
+   
+    socket.on('news_by_server', function(data){
+        if(data==0){
+            console.log(data, "no presionado");
+        }else{
+            console.log(data, "presionado");
+        }
+    });
+});
